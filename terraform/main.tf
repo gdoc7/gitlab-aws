c31c44a6e4d446f1db7bc87653e156feb059a57b
gabriel <gabrielojeda7@gmail.com>
2025-02-12 16:02:33 -0400

    Add Bastion host module configuration to Terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,9 @@ module "vpc" {
   private_subnets    = var.private_subnets
   enable_nat_gateway = var.enable_nat_gateway
 }
+
+module "bastion" {
+  source            = "../../modules/Bastion"
+  vpc_id            = module.vpc.vpc_id
+  public_subnets_ids = module.vpc.public_subnets_ids  
+}
